70970a05bd557f4cd05f99463f45dc8325c798ca
jamieliu2 <shanjliu@gmail.com>
2026-04-22 21:34:35 -0700

    added maximum mean matching for gp prediction

--- code/gp.py
+++ code/gp.py
@@ -6,14 +6,14 @@ from sklearn.exceptions import NotFittedError
 class GaussianProcess():
     def __init__(self, m, K, Sigma=None):
         '''
-        m : array(x, y, z) -> mean
-        K : array(x_i, y_i, z_i), array(x_j, y_j, z_j) -> cov
-        m : array(x, y, z) -> error variance
+        m : x_i -> mean
+        K : x_i, x_j-> cov
+        Sigma : x_i -> error variance
         '''
         self.m = jax.jit(jax.vmap(m))
         self.K = jax.jit(jax.vmap(jax.vmap(K, in_axes=(None, 0)), in_axes=(0, None)))
         if Sigma is None:
-            Sigma = lambda xyz: 1.0
+            Sigma = lambda x: 1.0
         self.Sigma = jax.jit(jax.vmap(Sigma))
 
         self.trained = False
@@ -25,6 +25,8 @@ class GaussianProcess():
         '''
         self.X_train = X_train
         self.y_train = y_train
+        self.d = X_train.shape[1]
+
         self.mn = self.m(X_train)
         self.Kn = self.K(X_train, X_train)
 
@@ -34,9 +36,35 @@ class GaussianProcess():
 
         self.trained = True
 
-    def predict(self, X_new):
+    def predict(self, X_new, candidates=None):
+        '''
+        X_new : n_new x (d - k)
+        candidates : m x k
+            for each row in X_new fill the last k columns with argmax_{j=1,...m} E[X_new, candidate_j | y]
+        '''
         if not self.trained:
             raise NotFittedError("This Gaussian Process instance has not been fitted.")
-        y_mean = self.m(X_new) + self.K(X_new, self.X_train) @ self.Kn_inv @ (self.y_train - self.mn)
-        y_cov = self.K(X_new, X_new) - self.K(X_new, self.X_train) @ self.Kn_inv @ self.K(self.X_train, X_new)
-        return y_mean, y_cov
+
+        def posterior_mean(X_new):
+            return self.m(X_new) + self.K(X_new, self.X_train) @ self.Kn_inv @ (self.y_train - self.mn)
+        def posterior_cov(X_new):
+            return self.K(X_new, X_new) - self.K(X_new, self.X_train) @ self.Kn_inv @ self.K(self.X_train, X_new)
+
+        if candidates is not None:
+            candidates = jnp.array(candidates)
+            m, k = candidates.shape
+            n, l = X_new.shape
+            if k > self.d:
+                raise ValueError("max_over array has incorrect shape to be maximized over.")
+            X_new = jnp.concatenate([X_new, jnp.zeros((n, self.d - l))], axis=1)
+
+            X_new_rep = jnp.repeat(X_new, m, axis=0)
+            candidates_tile = jnp.tile(candidates, (n,1))
+
+            X_new_filled = X_new_rep.at[:,-k:].set(candidates_tile)
+            scores = posterior_mean(X_new_filled).reshape(n, m)
+
+            best_candidates =  candidates[jnp.argmax(scores, axis=1)]
+            X_new = X_new.at[:,-k:].set(best_candidates)
+
+        return X_new, posterior_mean(X_new), posterior_cov(X_new)
