8db3dd7e780f495f3fa213d8957417df1493940d
jamieliu2 <shanjliu@gmail.com>
2026-05-03 00:54:27 -0700

    milestone3 exploration

--- code/gp.py
+++ code/gp.py
@@ -5,9 +5,10 @@ import jax.random as jr
 from functools import partial
 from sklearn.exceptions import NotFittedError
 
+STABILIZER = 1e-4
 
 @partial(jax.jit, static_argnames=['stabilizer'])
-def stable_cholesky(M, stabilizer=1e-8):
+def stable_cholesky(M, stabilizer=STABILIZER):
     '''
     Numerically stable Cholesky decomposition
     '''
@@ -28,7 +29,7 @@ def stable_cholesky(M, stabilizer=1e-8):
 
 
 @partial(jax.jit, static_argnames=['stabilizer'])
-def fast_inverse_cov(M, stabilizer=1e-8):
+def fast_inverse_cov(M, stabilizer=STABILIZER):
     '''
     Faster inverse of a positive semi-definite matrix
     '''
@@ -36,8 +37,8 @@ def fast_inverse_cov(M, stabilizer=1e-8):
     return jsp.linalg.cho_solve((L, True), jnp.eye(M.shape[0]))
 
 @partial(jax.jit, static_argnames=['p_m_cov', 'update_IG_in', 'update_IG_out', 'stabilizer'])
-def gibbs_step(key, X_train, y_train, Sigma_0,
-               p_m_cov, update_IG_in, update_IG_out, stabilizer=1e-8):
+def _gibbs_step(key, X_train, y_train, Sigma_0,
+               p_m_cov, update_IG_in, update_IG_out, stabilizer=STABILIZER):
     '''
     key: jax.random key
     cov_stabilizer: if cholesky decomp fails due to numerical issues, add diagonal matrix with cov_stabilizer
@@ -58,12 +59,12 @@ def gibbs_step(key, X_train, y_train, Sigma_0,
 
     sigma2_in = bn_in / jr.gamma(key_var_in, an_in)
     sigma2_out = bn_out / jr.gamma(key_var_out, an_out)
-    Sigma_0 = jnp.diag(jnp.where(X_train[:,-1] == 1, sigma2_in, sigma2_out))
+    Sigma_0 = jnp.where(X_train[:,-1] == 1, sigma2_in, sigma2_out)
 
-    return f_hat, Sigma_0, jnp.stack([an_in, bn_in, an_out, bn_out])
+    return f_hat, Sigma_0
 
 class GaussianProcess():
-    def __init__(self, m, K, cov_stabilizer=1e-8):
+    def __init__(self, m, K, cov_stabilizer=STABILIZER):
         '''
         m : x_i -> mean
         K : x_i, x_j-> cov
@@ -83,7 +84,7 @@ class GaussianProcess():
         priors : inverse gamma hyperparameters for indoor/outdoor components, a0_in, b0_in, a0_out, b0_out
         '''
         if Sigma_0 is None:
-            Sigma_0 = jnp.eye(X_train.shape[0]) * 1e-3 #jnp.var(y_train)
+            Sigma_0 = jnp.ones(X_train.shape[0]) * y_train.var()
         self.Sigma_0 = Sigma_0
 
         self.X_train = X_train
@@ -107,15 +108,15 @@ class GaussianProcess():
             return self.IG_priors[2] + (self.X_train[:,-1] == 0).sum()/2, self.IG_priors[3] + S_out/2
 
         @jax.jit
-        def posterior_mean_cov(X_new, Sigma_0):
-            L = stable_cholesky(self.Kn + Sigma_0, self.cov_stabilizer)
+        def posterior_mean_cov(X_new, Sigma_0, K_new):
+            L = stable_cholesky(self.Kn + jnp.diag(Sigma_0), self.cov_stabilizer)
             Kn_new = self.K(self.X_train, X_new)
 
             A = jsp.linalg.cho_solve((L, True), self.y_train - self.mn)
             m_post = self.m(X_new) + Kn_new.T @ A
 
             V = jsp.linalg.solve_triangular(L, Kn_new, lower=True)
-            cov_post = self.K(X_new, X_new) - V.T @ V
+            cov_post = K_new - V.T @ V
 
             return m_post, cov_post
 
@@ -123,84 +124,78 @@ class GaussianProcess():
         self.update_IG_out = update_IG_out
         self.posterior_mean_cov = posterior_mean_cov
 
-        @jax.jit
-        def sample_f_cond(key, X_new, Sigma_0):
-            mu_new, Sigma_new = self.posterior_mean_cov(X_new, Sigma_0)
-            sqrtCov = stable_cholesky(Sigma_new, self.cov_stabilizer)
-            return sqrtCov @ jr.normal(key, shape=mu_new.shape) + mu_new
-
-        @jax.jit
-        def sample_f_marg(key, X_new, E_Sigma, IG_posteriors):
-            # approximate since we use E_Sigma instead of integrating out
-            # partly vibe coded, needs double-checking later
-            mu_new, Sigma_new = self.posterior_mean_cov(X_new, E_Sigma)
-            indoor = X_new[:,-1]
-            sigma2_star = jnp.where(indoor,
-                            IG_posteriors[1] / (IG_posteriors[0] - 1),
-                            IG_posteriors[3] / (IG_posteriors[2] - 1))
-            df_new = jnp.where(indoor, 2*IG_posteriors[0], 2*IG_posteriors[2])
-            scale = jnp.sqrt(jnp.diag(Sigma_new) + sigma2_star)
-            key_n, key_c = jr.split(key)
-            Z = jr.normal(key_n, shape=mu_new.shape)
-            X2 = jr.chisquare(key_c, df=df_new, shape=mu_new.shape)
-            t = Z / jnp.sqrt(X2 / df_new)
-            return mu_new + scale*t
-
-        self._sample_f_cond = sample_f_cond
-        self._sample_f_marg = sample_f_marg
-
         self.trained = True
 
-    def gibbs(self, key):
+    def gibbs(self, key=jr.PRNGKey(305), chains=1, samples=20):
         '''
         key: jax.random key
         cov_stabilizer: if cholesky decomp fails due to numerical issues, add diagonal matrix with cov_stabilizer
             multiply stabilizer by 10 until cholesky decomposition works
         '''
-        new_key, gibbs_key = jr.split(key)
-        f_hat, Sigma_0, IG_posteriors = gibbs_step(gibbs_key,
-            self.X_train, self.y_train, self.Sigma_0, self.posterior_mean_cov,
-            self.update_IG_in, self.update_IG_out, self.cov_stabilizer)
-
-        self.f_hat = f_hat
-        self.Sigma_0 = Sigma_0
-        self.IG_posteriors = IG_posteriors
-        return new_key
-
-    def sample_f(self, key, X_new, conditional=False, Sigma_0=None):
-        if conditional:
-            if Sigma_0 is None: Sigma_0 = self.Sigma_0
-            return self._sample_f_cond(key, X_new, Sigma_0)
-        else:
-            sigma2_in = self.IG_posteriors[1] / (self.IG_posteriors[0] - 1)
-            sigma2_out = self.IG_posteriors[3] / (self.IG_posteriors[2] - 1)
-            E_Sigma = jnp.diag(jnp.where(self.X_train[:,-1] == 1, sigma2_in, sigma2_out))
-            return self._sample_f_marg(key, X_new, E_Sigma, self.IG_posteriors)
-
-    def predict(self, X_new, candidates=None):
-        '''
-        X_new : n_new x (d - k)
-        candidates : m x k
-            for each row in X_new fill the last k columns with argmax_{j=1,...m} E[X_new, candidate_j | y]
-        '''
         if not self.trained:
             raise NotFittedError("This Gaussian Process instance has not been fitted.")
 
-        if candidates is not None:
-            candidates = jnp.array(candidates)
-            m, k = candidates.shape
-            n, l = X_new.shape
-            if k > self.d:
-                raise ValueError("max_over array has incorrect shape to be maximized over.")
-            X_new = jnp.concatenate([X_new, jnp.zeros((n, self.d - l))], axis=1)
+        gibbs_step = partial(_gibbs_step, X_train=self.X_train, y_train=self.y_train,
+                                p_m_cov=partial(self.posterior_mean_cov, K_new=self.Kn),
+                                update_IG_in=self.update_IG_in, update_IG_out=self.update_IG_out)
 
-            X_new_rep = jnp.repeat(X_new, m, axis=0)
-            candidates_tile = jnp.tile(candidates, (n,1))
+        def single_chain_scan(Sigma_0, key):
+            f_hat_new, Sigma_0_new = gibbs_step(key=key, Sigma_0=Sigma_0)
+            return Sigma_0_new, (f_hat_new, Sigma_0_new)
 
-            X_new_filled = X_new_rep.at[:,-k:].set(candidates_tile)
-            scores, _ = self.posterior_mean_cov(X_new_filled, self.Sigma_0).reshape(n, m)
+        keys = jr.split(key, chains)
+        carry, gibbs_chains = jax.vmap(
+            lambda key: jax.lax.scan(single_chain_scan, init=self.Sigma_0, xs=jr.split(key, samples))
+        )(keys)
 
-            best_candidates =  candidates[jnp.argmax(scores, axis=1)]
-            X_new = X_new.at[:,-k:].set(best_candidates)
+        self.f_hat = gibbs_chains[0][0][-1]
+        self.Sigma_0 = gibbs_chains[1][0][-1]
 
-        return X_new, *self.posterior_mean_cov(X_new, self.Sigma_0)
+        S = (self.y_train - self.f_hat)**2
+        S_in  = jnp.sum(jnp.where(self.X_train[:,-1], S, 0.0))
+        S_out = jnp.sum(jnp.where(1-self.X_train[:,-1], 0.0, S))
+
+        an_in, bn_in = self.update_IG_in(S_in)
+        an_out, bn_out = self.update_IG_out(S_out)
+        self.IG_posteriors= jnp.array([an_in, bn_in, an_out, bn_out])
+
+        return gibbs_chains
+
+    def posterior(self, X_new, cov_chains):
+        K_new = self.K(X_new, X_new)
+        
+        def single_posterior(sigma2_vec):
+            m, cov = self.posterior_mean_cov(X_new, sigma2_vec, K_new)
+            return m, jnp.diag(cov)  # (n,) instead of (n, n)
+        
+        flat_chains = jnp.concatenate(cov_chains, axis=0)
+        means, vars = jax.lax.map(single_posterior, flat_chains)
+        return means, vars
+        
+    # def posterior(self, X_new, cov_chains):
+    #     K_new = self.K(X_new, X_new)
+    #     flat_chains = cov_chains.reshape(-1, cov_chains.shape[-1])
+    
+    #     def accumulate(carry, sigma2_vec):
+    #         m_sum, cov_sum, count = carry
+    #         m, cov = self.posterior_mean_cov(X_new, sigma2_vec, K_new)
+    #         return (m_sum + m, cov_sum + cov, count + 1), None
+    
+    #     init = (jnp.zeros(X_new.shape[0]),
+    #             jnp.zeros((X_new.shape[0], X_new.shape[0])),
+    #             0)
+    #     (m_sum, cov_sum, count), _ = jax.lax.scan(accumulate, init, flat_chains)
+    #     return m_sum / count, cov_sum / count
+    
+    def simulate(self, X_new, cov_chains, key=jr.PRNGKey(305)):
+        K_new = self.K(X_new, X_new)
+        def sample_conditional(args):
+            Sigma_0, key_f = args
+            m_post, cov_post = self.posterior_mean_cov(X_new, Sigma_0, K_new)
+            sqrtCov = stable_cholesky(cov_post)
+            return sqrtCov @ jr.normal(key_f, shape=m_post.shape) + m_post
+
+        return jax.vmap(lambda Sigma_0, key_c:
+            jax.lax.map(sample_conditional, (Sigma_0, jr.split(key_c, Sigma_0.shape[0])))
+                       )(cov_chains, jr.split(key, cov_chains.shape[0]))
+
