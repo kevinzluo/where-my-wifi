fixed RBF parametrization in gridsearch and gibbs sampler, updated gp.py predict to kill temporal covariance:

code/gp.py
@@ -132,8 +132,9 @@ class GaussianProcess():
         return gibbs_chains
 
     def predict(self, X_new, cov_chains, method='parallel'):
-        K_new = self.K(X_new, X_new)
-        Kn_new = self.K(self.X_train, X_new)
+        # note: set t_new to be infinity to kill temporal covariance
+        K_new = self.K(X_new, X_new.at[:,3].set(jnp.inf))
+        Kn_new = self.K(self.X_train, X_new.at[:,3].set(jnp.inf))
 
         @jax.jit
         def single_posterior(variances):

code/gridsearch/gibbs_gs.py
@@ -46,7 +46,7 @@ def m(obs):
 def K(obs1, obs2):
     d_xyz = (obs1[:3] - obs2[:3])**2
     d_t = (obs1[3] - obs2[3])**2
-    return os_xyz * jnp.exp(- (d_xyz / ls_xyz**2).sum()) + \
+    return os_xyz * jnp.exp(- (d_xyz / (2*ls_xyz**2)).sum()) + \
         os_t * jnp.exp(- d_t / (2*ls_t**2))
 
 ### RUN GP ###