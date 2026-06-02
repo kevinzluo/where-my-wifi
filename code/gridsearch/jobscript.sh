67ab2f359206caa12a7fc66cd4feb93893143519
jamieliu2 <shanjliu@gmail.com>
2026-06-02 14:39:00 -0700

    minor fixes

--- code/gridsearch/jobscript.sh
+++ code/gridsearch/jobscript.sh
@@ -2,6 +2,6 @@
 #SBATCH --job-name=gp_grid
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus=1
-#SBATCH --array=138,175,229,313,343,394,437,456,517,519,530
+#SBATCH --array=0
 
 conda run -n jax-cuda12 python "gibbs_gs.py" $SLURM_ARRAY_TASK_ID
